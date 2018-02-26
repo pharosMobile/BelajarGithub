1982cb9dee5d57935c704263c00571e3a4523a73
Jason <jasonnathanael18@gmail.com>
2018-02-26 12:05:21 +0700

    tambahin toast

--- app/src/main/java/com/example/administrator/belajargithub/MainActivity.java
+++ app/src/main/java/com/example/administrator/belajargithub/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this, "halooooo", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "halooooo", Toast.LENGTH_SHORT).show();
     }
 }
