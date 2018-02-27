b7209bd59cec787df4c2d88ceae169c55c222717
pharosmobile <forpharosmobile@gmail.com>
2018-02-27 10:01:16 +0700

    first

---
+++ app/src/main/java/com/example/administrator/belajargithub/KeduaActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.belajargithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KeduaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kedua);
+    }
+}
