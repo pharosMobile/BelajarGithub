f5a7b5d3012f64062b65aa9c6de34e360b854bd7
Jason <jasonnathanael18@gmail.com>
2018-02-27 10:27:28 +0700

    tambahin toast di activityvketiga

--- app/src/main/java/com/example/administrator/belajargithub/KetigaActivity.java
+++ app/src/main/java/com/example/administrator/belajargithub/KetigaActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.belajargithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class KetigaActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class KetigaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ketiga);
+        Toast.makeText(this, "vwrgthtgeas", Toast.LENGTH_SHORT).show();
     }
 }
